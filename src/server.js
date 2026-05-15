d5f613dd77e9304b53cc2887d15392cc68efe543
Felps-GS <felipeg.desouza4@gmail.com>
2026-05-15 09:48:04 -0300

    Adição do metodo post, put e delete

--- src/server.js
+++ src/server.js
@@ -27,6 +27,61 @@ app.get('/alunos', async (req, res) => {
   }
 });
 
+app.post('/alunos', async(req, res) =>{
+  try{
+    const {nome, RA} = req.body;
+    if(!nome || !RA){
+      return res.status(400).json({
+        mensagem: 'Os campos nome e curso sao obrigatorios.'
+      });
+    }
+    const novosAlunos = await Aluno.create({nome, RA});
+    res.status(201).json(novosAlunos);
+  } catch(error){
+
+  }
+});
+
+app.put('/alunos/:id', async(req,res) =>{
+  try{
+    const {id} = req.params
+    const{nome, RA} = req.body;
+    const aluno = await Aluno.findByIdAndUpdate(id, {nome, RA}, {new: true, runValidators: true});
+    if(!aluno){
+      return res.status(404).json({
+        mensagem: "Não foi encotrado o aluno"
+      });
+    }
+    res.status(200).json(aluno); 
+  }catch(error){
+    res.status(500).json({
+      mensagem: 'Erro ao atualizar aluno.',
+      erro: error.message
+    });
+  }
+});
+
+app.delete('/alunos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const aluno = await Aluno.findById(id);
+    if (!aluno) {
+      return res.status(404).json({
+        mensagem: 'Aluno não encontrado.'
+      });
+    }
+    await Aluno.findByIdAndDelete(id);
+    res.status(200).json({
+      mensagem: 'Aluno removido com sucesso.'
+    });
+  } catch (error) {
+    res.status(500).json({
+      mensagem: 'Erro ao remover aluno.',
+      erro: error.message
+    });
+  }
+});
+
 async function startServer() {
   try {
     await connectDatabase();
